fe0c5e22c8e77ae799f4b80f27d1a10a685c36a4
Davidrodrguez98 <50140333+Davidrodrguez98@users.noreply.github.com>
2021-09-29 10:35:53 +0200

    Rendering HTML template

--- app/app.py
+++ app/app.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # return "<h1>weee</h1>"
-    return render_template('index.html')
+    courses = ['PHP', 'Django', 'Python', 'Flask', 'Java']
+    data = {
+        'title': 'Index',
+        'welcome': 'Hi!',
+        'courses': courses,
+        'n_courses': len(courses)
+    }
+    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
